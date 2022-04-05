d5dcf06982e57feaa0c3726b7518139d7b15397a
Christian Riccio <riccio.christian.21@gmail.com>
2022-04-05 12:48:56 +0200

    version 1.0.1
    new changes, appIcon and swiftUI

--- InsuranceCosts/ContentView.swift
+++ InsuranceCosts/ContentView.swift
@@ -9,52 +9,84 @@ import SwiftUI
 import CoreML
 
 struct ContentView: View {
+    var sexs = ["Female", "Male"]
+    var smokes = ["Yes", "No"]
+    @State private var height: Float = 0.0
+    @State private var weight: Float = 0.0
     @State private var age: Double = 0
     @State private var isEditingAge = false
-    @State private var sex = ""
+    @State private var sex = "Female"
     @State private var children: Double = 0
-    @State private var smoke = ""
+    @State private var smoke = "Yes"
     @State private var bmi: Double = 0
-    @State private var isEditingBmi = false
-    
     @State private var altertTitle = ""
     @State private var alertMessage=""
     @State private var isShowing = false
-    
+    @State var a: String = ""
     var body: some View {
-            Form {
+        NavigationView {
+            VStack(spacing:20){
                 HStack{
-                    Text("Sex").font(.headline).padding()
-                    TextField("male or female",text: $sex).padding()
+                    Text("Sex").font(.headline)
+                Picker("Sex: ", selection: $sex){
+                    ForEach(sexs, id: \.self){
+                        Text($0)
+                            .font(.headline)
+                    }
+                }.padding(.trailing,290)
                 }
                 HStack{
-                    Stepper("\(age.formatted()) years old", value: $age, in: 0...100, step: 0.5).padding()
-                }
-                Stepper(children == 0 ? "child": "\(children.formatted()) children", value: $children, in: 0...10, step: 1).padding()
-                HStack{
-                    Text("Smoke").font(.headline).padding()
-                    TextField("yes or no",text: $smoke).padding()
-                }
-                HStack{
-                    Text("BMI").font(.headline).padding()
-                    Slider(value: $bmi, in: 0...100,
-                           onEditingChanged: { editing in
-                        isEditingBmi = editing}).padding()
-                    Text("\(bmi.formatted())")
-                        .foregroundColor(isEditingAge ? .red : .black).padding()
-                }
+                    Text("Age")
+                    TextField( "\(age.formatted())", value: $age, format: .number)
+//                    Stepper("\(age.formatted())", in: 0...100, step: 0.5)
+                    
+                }.padding()
+
+            Stepper(children == 1 ? "child": "\(children.formatted()) children", value: $children, in: 0...10, step: 1).padding()
+            HStack{
+                Text("Smoke").font(.headline)
+                Picker("Smoke: ", selection: $smoke){
+                    ForEach(smokes, id: \.self){
+                        Text($0)
+                            .font(.headline)
+                    }
+                }.padding(.trailing,290)
+                
             }
-        Button("Calculate Insurance Costs", action: calculateCosts).font(.headline).padding().alert(altertTitle, isPresented: $isShowing){
-            Button("ok"){ }
-        } message: {
-            Text("\(alertMessage) $")
+            VStack{
+                HStack{
+                    Text("Weight (kg)").padding()
+                TextField("Insert your weight: ", value: $height, format: .number)
+                }
+                HStack{
+                    Text("Height (cm)").padding()
+                TextField("Insert your height: ", value: $weight, format: .number)
+                }
+
+                Button("Calculate BMI", action: {
+                   a = calcBmi(weightInKG: weight, heightInCM: height)
+                })
+                Text("\(a)")
+                
+            }
+                
+                Spacer()
+                
+                Button("Calculate Healtcare Costs", action: calculateCosts).font(.headline).padding().alert(altertTitle, isPresented: $isShowing){
+                    Button("ok"){ }
+                } message: {
+                    Text("\(alertMessage) $")
+                }
+            }.navigationTitle("Healthcare Costs").font(.headline)
         }
-    }
+        
+        }
+
     func calculateCosts() {
         do {
             let config = MLModelConfiguration()
             let model = try MedicalCosts(configuration: config)
-            let prediction =  try model.prediction(age: age, sex: sex, bmi: bmi, children: children, smoker: smoke)
+            let prediction =  try model.prediction(age: Double(age), sex: sex, bmi: Double(a)!, children: children, smoker: smoke)
             let charges = prediction.charges.formatted()
             print("estimated insurance charges: \(prediction.charges.formatted())")
             altertTitle = "Your estimated insurance costs are:"
@@ -65,10 +97,10 @@ struct ContentView: View {
         }
         isShowing = true
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func calcBmi(weightInKG:Float, heightInCM: Float)->String{
+        let bmi = weightInKG / ((heightInCM * heightInCM )/1000)
+        return String(round(bmi))
     }
 }
+
+
--- InsuranceCosts/InsuranceCostsApp.swift
+++ InsuranceCosts/InsuranceCostsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InsuranceCostsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
